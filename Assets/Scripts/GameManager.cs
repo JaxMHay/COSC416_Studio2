f5f2872447f2f5a67b3eb7c9a61f9fc03069b2eb
JaxMHay <jaxonmhay@gmail.com>
2025-02-14 20:46:06 -0800

    Score Implemented

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private float score = 0;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    private FallTrigger[] pins;
+
+
+    private void Start()
+    {
+        // Correct the FindObjectsByType method call
+        pins = FindObjectsByType<FallTrigger>(FindObjectsSortMode.None);
+
+        foreach (FallTrigger pin in pins)
+        {
+            pin.OnPinFall.AddListener(IncrementScore);
+        }
+    }
+
+    private void IncrementScore()
+    {
+        score++;
+        scoreText.text = $"Score: {score}";
+    }
+}
